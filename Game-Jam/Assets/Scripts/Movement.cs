c7517ce041a4c7d795710f75759fe7e411e85401
Szorader <michalgorecki2004@o2.pl>
2024-11-19 13:07:56 +0100

    half animation

--- Game-Jam/Assets/Scripts/Movement.cs
+++ Game-Jam/Assets/Scripts/Movement.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System;
 
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f; // speed
 
     private Rigidbody2D rb;
+    public Animator animator;
     private Vector2 movement;
 
-    public bool isInputBlocked = false;
-
+    private int newX;
+    private int newY;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -29,6 +32,49 @@ public class Movement : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         movement = movement.normalized;
+
+        if( movement.x > 0.5 && movement.x < 1){
+            newX = 2;
+        }
+        if(movement.x < -0.5 && movement.x > -1){
+            newX = -2;
+        }
+        if( movement.x == 1){
+            newX = 1;
+        }
+        if(movement.x == -1){
+            newX = -1;
+        }
+        if( movement.x == 0){
+            newX = 0;
+        }
+        if( movement.y > 0.5 && movement.y < 1){
+            newY = 2;
+        }
+        if(movement.y < -0.5 && movement.y > -1){
+            newY = -2;
+        }
+        if( movement.y == 1){
+            newY = 1;
+        }
+        if(movement.y == -1){
+            newY = -1;
+        }
+        if(movement.y == 0){
+            newY = 0;
+        }
+        
+        Debug.Log("x");
+        Debug.Log(newX);
+        Debug.Log("y");
+        Debug.Log(newY);
+
+        animator.SetInteger("MoveX", newX);
+        animator.SetInteger("MoveY", newY);
+        animator.SetBool("IsMoving", movement.sqrMagnitude > 0);
+
+
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 
     void FixedUpdate()
